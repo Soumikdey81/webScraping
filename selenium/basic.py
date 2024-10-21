303f97252ac38e1f9f4bdc4333ee27aea843ea14
Soumikdey81 <aditdey30@gmail.com>
2024-10-21 12:29:44 +0530

    Create basic.py

---
+++ selenium/basic.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+path = r'C:\Users\soumi_mu7w4\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe'
+driver = webdriver.chrome(path)
